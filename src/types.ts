fe8d994274aade0494a99d4f5d0e5adf87af9122
Trent Jones <trentjones21@gmail.com>
2018-12-26 15:52:13 -0700

    updates

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,20 @@
+export interface Money {
+  fromFloat(): Money;
+  fromString(): Money;
+  valueOf(): Number;
+  toString(): String;
+  toUsd(): String;
+  plus(other: Money): Money;
+  minus(other: Money): Money;
+  times(other: Number): Money;
+  divideBy(other: Number): Money;
+  greaterThan(other: Money): Boolean;
+  greaterThanOrEqual(other: Money): Boolean;
+  lessThan(other: Money): Boolean;
+  lessThanOrEqual(other: Money): Boolean;
+  max(other: Money): Money;
+  min(other: Money): Money;
+}
 export class Money {
   fromFloat(): Money;
   fromString(): Money;
